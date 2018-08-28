3aa6064c33f933116421ad38405b167324df7625
Alejandra Gomez <alejita.9813.dg@gmail.com>
2018-08-28 17:58:18 -0500

    avance

--- src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
+++ src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
@@ -19,10 +19,14 @@ package edu.eci.arsw.myrestaurant.restcontrollers;
 import edu.eci.arsw.myrestaurant.model.Order;
 import edu.eci.arsw.myrestaurant.model.ProductType;
 import edu.eci.arsw.myrestaurant.model.RestaurantProduct;
+import edu.eci.arsw.myrestaurant.services.RestaurantOrderServices;
 import edu.eci.arsw.myrestaurant.services.RestaurantOrderServicesStub;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,7 +38,23 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author hcadavid
  */
+@RestController
+@RequestMapping(value = "/orders")
 public class OrdersAPIController {
-
     
+        @Autowired
+        private RestaurantOrderServices ros;
+
+ 	@RequestMapping(method = RequestMethod.GET)
+ 	public ResponseEntity<?> manejadorGetRecursoOrdersAPI() {
+        try {
+            //obtener datos que se enviarán a través del API
+            return new ResponseEntity<>(ros.getTablesWithOrders(), HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
+            Logger.getLogger(OrdersAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error, no se encontraron mesas", HttpStatus.NOT_FOUND);
+        }
+    }    
 }
+    
+
